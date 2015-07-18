81fa09c02871452bbcd39f98212a860e2304b6c9
root <root@iZ2326jh041Z.(none)>
2015-07-18 16:55:13 +0800

    .

---
+++ js/custom.js
@@ -0,0 +1,67 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Place here your custom scripts
+ */
+var get_time_difference = function () 
+{
+    var earlierDate = new Date();
+    var laterDate = new Date(2015, 4, 30, 9, 0, 0, 0);
+    var oDiff = new Object();
+
+    //  Calculate Differences
+    var nTotalDiff = laterDate.getTime() - earlierDate.getTime();
+
+    oDiff.days = Math.floor(nTotalDiff / 1000 / 60 / 60 / 24);
+    nTotalDiff -= oDiff.days * 1000 * 60 * 60 * 24;
+
+    oDiff.hours = Math.floor(nTotalDiff / 1000 / 60 / 60);
+    nTotalDiff -= oDiff.hours * 1000 * 60 * 60;
+
+    oDiff.minutes = Math.floor(nTotalDiff / 1000 / 60);
+    nTotalDiff -= oDiff.minutes * 1000 * 60;
+
+    oDiff.seconds = Math.floor(nTotalDiff / 1000);
+    //  -------------------------------------------------------------------
+
+    //  Format Duration
+    //  -------------------------------------------------------------------
+    //  Format Hours
+    var hourtext = '00';
+    if (oDiff.days > 0){ hourtext = String(oDiff.days);}
+    if (hourtext.length == 1){hourtext = '0' + hourtext};
+
+    //  Format Minutes
+    var mintext = '00';
+    if (oDiff.minutes > 0){ mintext = String(oDiff.minutes);}
+    if (mintext.length == 1) { mintext = '0' + mintext };
+
+    //  Format Seconds
+    var sectext = '00';
+    if (oDiff.seconds > 0) { sectext = String(oDiff.seconds); }
+    if (sectext.length == 1) { sectext = '0' + sectext };
+
+    //  Set Duration
+    var sDuration = hourtext + ':' + mintext + ':' + sectext;
+    oDiff.duration = sDuration;
+    //  -------------------------------------------------------------------
+
+    return oDiff;
+};
+
+$(function(){
+    var cddays = $(".cddays");
+    var cdhours = $(".cdhours");
+    var cdminutes = $(".cdminutes");
+    var cdseconds = $(".cdseconds");
+    setInterval(function(){
+	    var diff = get_time_difference();
+	    cddays.text(diff.days);
+	    cdhours.text(diff.hours);
+	    cdminutes.text(diff.minutes);
+	    cdseconds.text(diff.seconds);
+	}, 1000);
+});
---
+++ js/template.js
@@ -0,0 +1,203 @@
+/* Theme Name: Worthy - Free Powerful Theme by HtmlCoder
+ * Author:HtmlCoder
+ * Author URI:http://www.htmlcoder.me
+ * Version:1.0.0
+ * Created:November 2014
+ * License: Creative Commons Attribution 3.0 License (https://creativecommons.org/licenses/by/3.0/)
+ * File Description: Initializations of plugins
+ */
+
+(function($){
+	$(document).ready(function(){
+
+		//$(".banner-image").backstretch('images/banner.jpg');
+
+		// Fixed header
+		//-----------------------------------------------
+		$(window).scroll(function() {
+			if (($(".header.fixed").length > 0)) {
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+		$(window).load(function() {
+			if (($(".header.fixed").length > 0)) {
+				if(($(this).scrollTop() > 0) && ($(window).width() > 767)) {
+					$("body").addClass("fixed-header-on");
+				} else {
+					$("body").removeClass("fixed-header-on");
+				}
+			};
+		});
+
+		//lazy img
+        $("img[data-original]").lazyload({
+		    skip_invisible : false
+		});
+
+		//Scroll Spy
+		//-----------------------------------------------
+		if($(".scrollspy").length>0) {
+			$("body").addClass("scroll-spy");
+			$('body').scrollspy({
+				target: '.scrollspy',
+				offset: 152
+			});
+		}
+
+		//Smooth Scroll
+		//-----------------------------------------------
+		if ($(".smooth-scroll").length>0) {
+			$('.smooth-scroll a[href*=#]:not([href=#]), a[href*=#]:not([href=#]).smooth-scroll').click(function() {
+				if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+					var target = $(this.hash);
+					var that = this;
+					target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top-151
+						}, 600, function(){
+					        var target = $(that.hash);
+					        target = target.length ? target : $('[name=' + that.hash.slice(1) +']');
+					        if (target.length) {
+					        	$('html,body').animate({
+					        		scrollTop: target.offset().top-151
+					        	}, 400, function(){
+
+
+					        	});
+					        	return false;
+					        }
+
+						});
+						return false;
+					}
+				}
+			});
+		}
+
+		// Animations
+		//-----------------------------------------------
+		if (($("[data-animation-effect]").length>0) && !Modernizr.touch) {
+			$("[data-animation-effect]").each(function() {
+				var $this = $(this),
+				animationEffect = $this.attr("data-animation-effect");
+				if(Modernizr.mq('only all and (min-width: 768px)') && Modernizr.csstransitions) {
+					$this.appear(function() {
+						setTimeout(function() {
+							$this.addClass('animated object-visible ' + animationEffect);
+						}, 200);
+					}, {accX: 0, accY: -130});
+				} else {
+					$this.addClass('object-visible');
+				}
+			});
+		};
+
+		// Isotope filters
+		//-----------------------------------------------
+		if ($('.isotope-container').length>0) {
+			$(window).load(function() {
+				$('.isotope-container').fadeIn();
+				var $container = $('.isotope-container').isotope({
+					itemSelector: '.isotope-item',
+					layoutMode: 'masonry',
+					transitionDuration: '0.6s',
+					filter: ".2015ptf",
+                    onLayout: function() {
+                        $(window).trigger("scroll");
+                    }
+				});
+				// filter items on button click
+				$('.filters').on( 'click', 'ul.nav li a', function() {
+					var filterValue = $(this).attr('data-filter');
+					$(".filters").find("li.active").removeClass("active");
+					$(this).parent().addClass("active");
+					$container.isotope({ filter: filterValue });
+					return false;
+				});
+			});
+		};
+
+		//Modal
+		//-----------------------------------------------
+		var youkuIframe = function(id) {
+        	return ('<iframe height="100%" width="100%" src="http://player.youku.com/embed/' + id + '" frameborder=0 allowfullscreen></iframe>');
+        };
+		if($(".modal").length>0) {
+			$(".modal").each(function() {
+				$(".modal").prependTo( "body" );
+			});
+		}
+
+		$('body .video-modal').each(function(){
+		    var youkuId = $(this).attr('youkuid');
+			//console.log(youkuId);
+			$(this).on('show.bs.modal', function (e) {
+			    $(".modal-content", this).html(youkuIframe(youkuId));
+			}).on('hidden.bs.modal', function(e) {
+        	    $(".modal-content", this).html("");
+            });
+		});
+		// Owl Carousel Settings
+
+		$("#owl-example").owlCarousel();
+        $(".about-carousel").owlCarousel({
+            items: 4,
+			lazyLoad : true
+        });
+
+        $(".portfolio-carousel").owlCarousel({
+            singleItem: true,
+            navigation: true,
+            pagination: false,
+            navigationText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            autoHeight: true,
+            mouseDrag: true,
+            touchDrag: true,
+            //transitionStyle: "fadeUp",
+            loop:true,
+            autoPlay:true
+        });
+
+        $(".testimonials-carousel").owlCarousel({
+            singleItem: true,
+            navigation: true,
+            pagination: true,
+            autoHeight: true,
+            navigationText: [
+                "<i class='fa fa-angle-left'></i>",
+                "<i class='fa fa-angle-right'></i>"
+            ],
+            transitionStyle: "backSlide"
+        });
+
+        $(".portfolio-gallery").owlCarousel({
+            items: 3,
+        });
+		(function(){
+        	var my_posts = $("[rel=tooltip]");
+
+        	var size = $(window).width();
+        	for(i=0;i<my_posts.length;i++){
+        		the_post = $(my_posts[i]);
+
+        		if(the_post.hasClass('invert') && size >=767 ){
+        			the_post.tooltip({ placement: 'left'});
+        			the_post.css("cursor","pointer");
+        		}else{
+        			the_post.tooltip({ placement: 'rigth'});
+        			the_post.css("cursor","pointer");
+        		}
+        	}
+        })();
+
+	}); // End document ready
+})(this.jQuery);
